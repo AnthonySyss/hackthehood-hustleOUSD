61e53688078c5d1b311e2c87044fc771e8070d5a
Anthony Su <anthony@Anthonys-MacBook-Pro.local>
2025-06-17 16:51:03 -0700

    completed lab 2

---
+++ lab2.py
@@ -0,0 +1,35 @@
+# Lab 2: Python Profiles
+
+## Step 1
+name = "Anthony Su"
+
+## Step 2
+nickname = 'Ant'
+
+## Step 3
+age = 16.0
+
+## Step 4
+used_python = True//False
+
+## Step 5
+hobbies = ["gaming","playing sports","food"]
+
+## Step 6
+favorite_things = {"show": "SnowFall", "food": "Sushi",}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## int data type examples: 25, 22, 0 ,-1, -1134241231, 423213231
+## float data type examples: 25.0, 22.5, 0.0, -1.0, -1134241231.0, 423213231.0, 0.0
